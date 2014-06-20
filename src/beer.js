94443bf117500b724b143f26d17caaafea505509
Joshua Stupplebeen <jpstup@gmail.com>
2014-06-20 12:08:45 -0600

    added remove buttons

--- src/beer.js
+++ src/beer.js
@@ -1,9 +1,16 @@
 Ingredient = {}
 
+var ingredientHead = ('<div class="row"><p>').concat('<input type="button" id="remove" value="X">');
+
 Ingredient.add = function(context, data) {
-    $('<p>'+data+'</p>').appendTo(context).text(data);
+    $(ingredientHead.concat(data).concat('<p></div>')).appendTo(context);
 }
 
 $('#new').click(function() {
     Ingredient.add($('body'),$('#ingredient').val());
+});
+
+$('body').on('click', 'div.row', function(e) {
+    $(this).closest('div.row').remove();
+    e.preventDefault();
 });
