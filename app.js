99b79f6259f350e8136d4787203b0cdb1eacad81
Joshua Stupplebeen <jpstup@gmail.com>
2014-06-22 02:31:49 -0600

    deploy work

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var app = express();
 app.use('/', express.static(__dirname + '/src'));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() { 
-    console.log('Your files will be served through this web server')
+    console.log('Web server started')
 });
