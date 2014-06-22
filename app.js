42e614a6ede56e6f98bb3b1cb0670ae5a92bb342
Joshua Stupplebeen <jpstup@gmail.com>
2014-06-22 02:00:40 -0600

    trying to get deploy working

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-window.location.replace("./src/index.html");
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/src'));
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() { 
+    console.log('Your files will be served through this web server')
+});
